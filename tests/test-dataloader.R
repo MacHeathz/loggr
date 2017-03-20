be7e8f5a633c53a6f2cc8f2f8de63d095c075e54
Florian Ockhuysen <ockhuysen@yahoo.com>
2017-03-21 00:18:11 +0100

    small typos

--- tests/test-dataloader.R
+++ tests/test-dataloader.R
@@ -19,12 +19,12 @@ test_that("read_regexp of missing is missing.", {
   expect_that(read_regexp(NA, "^[^#]"), equals(NA))
 })
 
-test_that("read_regexp for empty string regexp return everything as a character vector.", {
+test_that("read_regexp for empty string regexp returns everything as a character vector.", {
   expect_that(read_regexp(string, ""), equals(c("# Comment 1", "2013-09-23, 23:23, sadf, sdf, 234", "# Comment 2")))
   expect_that(class(read_regexp(string, "")), equals("character"))
 })
 
-test_that("read_regexp passes extra parameters to read_lines function", {
+test_that("read_regexp passes extra parameters to read_lines function.", {
   expect_that(read_regexp(string, "", n_max = 2), equals(c("# Comment 1", "2013-09-23, 23:23, sadf, sdf, 234")))
   expect_that(read_regexp(string, "^[#]", skip = 1), equals("# Comment 2"))
 })
