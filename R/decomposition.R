4c5fa128d96aeba6591ffa5a0aebb6c7ad4ec09b
Florian Ockhuysen <ockhuysen@yahoo.com>
2017-04-09 12:34:18 +0200

    Wrap lines at 80 chars + other style changes

--- R/decomposition.R
+++ R/decomposition.R
@@ -1,9 +1,9 @@
 
-datastructure <- function (str) {
+datastructure <- function(str) {
   yaml::yaml.load_file(str)
 }
 
-extract_info <- function (str, regexps, values) {
+extract_info <- function(str, regexps, values) {
   stopifnot(length(regexps) == length(values))
 
   matrix <- sapply(seq_len(length(regexps)), function(i) {
@@ -13,11 +13,11 @@ extract_info <- function (str, regexps, values) {
   combine_values(matrix)
 }
 
-map_regexp_value <- function (str, regexp, value) {
+map_regexp_value <- function(str, regexp, value) {
   ifelse(stringr::str_detect(str, regexp), value, NA)
 }
 
-combine_values <- function (matrix) {
+combine_values <- function(matrix) {
   if (class(matrix) != "matrix") {
     matrix <- as.matrix(matrix)
   }
@@ -25,14 +25,14 @@ combine_values <- function (matrix) {
   combine_values_reduce(matrix)
 }
 
-combine_values_reduce <- function (matrix) {
+combine_values_reduce <- function(matrix) {
   Reduce(
     fold_na,
     m_to_v(t(matrix))
   )
 }
 
-combine_values_map <- function (matrix) {
+combine_values_map <- function(matrix) {
   unlist(
     lapply(
       Map(roll_na, m_to_v(matrix)),
@@ -40,7 +40,7 @@ combine_values_map <- function (matrix) {
     use.names = FALSE)
 }
 
-combine_values_apply <- function (matrix) {
+combine_values_apply <- function(matrix) {
   unlist(
     lapply(
       apply(matrix, 1, roll_na),
@@ -61,20 +61,20 @@ null_to_na <- function(arg) {
   ifelse(is.null(arg), NA, arg)
 }
 
-random.matrix <- function (arity) {
+random.matrix <- function(arity) {
   M <- Matrix::Diagonal(arity)
   Ms <- as.character(M)
   Ms[Ms == "0"] <- NA
   matrix(Ms, nrow = arity)
 }
 
-random.longmatrix <- function (m,n) {
+random.longmatrix <- function(m,n) {
   R <- random.matrix(n)
-  R <- matrix(rep(R, as.integer((m / n) + 1)), ncol=n, byrow=TRUE)
-  utils::head(R, n=m)
+  R <- matrix(rep(R, as.integer((m / n) + 1)), ncol = n, byrow = TRUE)
+  utils::head(R, n = m)
 }
 
-benchmark_combine_values <- function (m, n) {
+benchmark_combine_values <- function(m, n) {
   if (requireNamespace("rbenchmark", quietly = TRUE)) {
     R <- random.longmatrix(m, n)
     rbenchmark::benchmark(combine_values_reduce(R),
--- R/read_regexp.R
+++ R/read_regexp.R
@@ -1,39 +1,49 @@
-#' Read a file line by line, only return lines that match the provided regular
+#' Read a file using regexp.
+#'
+#' Read a file or string line by line, only return lines that match the provided regular
 #' expression.
 #'
-#' @param file Path to the file
+#' Paragraph shown after arguments with more details
+#'
+#' Another paragraph laying down the details
+#'
+#' @param file Path to the file or string
 #' @param regexp The regular expression
 #' @param ... Remaining arguments will be passed to the readr::read_lines method
 #' @return Character vector of lines
-read_regexp <- function (file, regexp, ...) {
-  if (is.na(file)) return (NA)
+#' @examples
+#' read_regexp(c("# comment", "logline"), "^[#]")
+read_regexp <- function(file, regexp, ...) {
+  if (is.na(file)) return(NA)
 
   lines <- readr::read_lines(file, ...)
   subset(lines, grepl(regexp, lines))
 }
 
-read_regexp_strdetect <- function (file, regexp, ...) {
-  if (is.na(file)) return (NA)
+read_regexp_strdetect <- function(file, regexp, ...) {
+  if (is.na(file)) return(NA)
 
   lines <- readr::read_lines(file, ...)
   subset(lines, stringr::str_detect(lines, regexp))
 }
 
-benchmark_read_regexp <- function () {
+benchmark_read_regexp <- function() {
   if (requireNamespace("rbenchmark", quietly = TRUE)) {
     rbenchmark::benchmark(
       lapply(
-        list.files("C:/Users/datalab1/Documents/Datalab/Schipholtunnel Casus/Data/Logfiles Schipholtunnel/2015 txt/",
-                   "Rapp.*.txt",
+        list.files(paste0("C:/Users/datalab1/Documents/Datalab/",
+                          "Schipholtunnel Casus/Data/Logfiles Schipholtunnel/",
+                          "2015 txt/Rapp.*.txt"),
                    full.names = T),
-        FUN=function(file){
+        FUN = function(file) {
           read_regexp(file, "^\\d{2}-\\d{2}-\\d{4}")
           }),
       lapply(
-        list.files("C:/Users/datalab1/Documents/Datalab/Schipholtunnel Casus/Data/Logfiles Schipholtunnel/2015 txt/",
-                   "Rapp.*.txt",
+        list.files(paste0("C:/Users/datalab1/Documents/Datalab/",
+                          "Schipholtunnel Casus/Data/Logfiles Schipholtunnel/",
+                          "2015 txt/Rapp.*.txt"),
                    full.names = T),
-        FUN=function(file){
+        FUN = function(file) {
           read_regexp_strdetect(file, "^\\d{2}-\\d{2}-\\d{4}")
           })
     )
